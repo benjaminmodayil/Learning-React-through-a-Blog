bc540adae3d2f419cd58c714a3b5883ee16cc92e
Benjamin Modayil <modayilme@gmail.com>
2017-07-10 11:17:53 -0400

    dffasdf

--- src/javascripts/routes.js
+++ src/javascripts/routes.js
@@ -1,9 +1,6 @@
-import React     from 'react'
-import {
-  Route,
-  IndexRoute
-} from 'react-router'
-import App       from './presenters/application'
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+import App from './presenters/application'
 import Blog from './components/blog/Blog'
 import Article from './components/blog/article/Article'
 import Chat from './components/Chat'
